30602b7937a15fb505f8696b603e7cecbf0840a1
Killen911 <30373628+Killen911@users.noreply.github.com>
2020-07-31 23:07:27 +0300

    Add files via upload

---
+++ democracy_and_gdp_per_capita.py
@@ -0,0 +1,788 @@
+# gdp per capita PPP 2018
+# democracy index 2018
+
+countries = """Aruba
+Afghanistan
+Angola
+Albania
+Andorra
+Arab World
+United Arab Emirates
+Argentina
+Armenia
+American Samoa
+Antigua and Barbuda
+Australia
+Austria
+Azerbaijan
+Burundi
+Belgium
+Benin
+Burkina Faso
+Bangladesh
+Bulgaria
+Bahrain
+Bahamas, The
+Bosnia and Herzegovina
+Belarus
+Belize
+Bermuda
+Bolivia
+Brazil
+Barbados
+Brunei Darussalam
+Bhutan
+Botswana
+Central African Republic
+Canada
+Central Europe and the Baltics
+Switzerland
+Channel Islands
+Chile
+China
+Cote d'Ivoire
+Cameroon
+Congo, Dem. Rep.
+Congo, Rep.
+Colombia
+Comoros
+Cabo Verde
+Costa Rica
+Caribbean small states
+Cuba
+Curacao
+Cayman Islands
+Cyprus
+Czech Republic
+Germany
+Djibouti
+Dominica
+Denmark
+Dominican Republic
+Algeria
+East Asia & Pacific (excluding high income)
+Early-demographic dividend
+East Asia & Pacific
+Europe & Central Asia (excluding high income)
+Europe & Central Asia
+Ecuador
+Egypt
+Euro area
+Eritrea
+Spain
+Estonia
+Ethiopia
+European Union
+Fragile and conflict affected situations
+Finland
+Fiji
+France
+Faroe Islands
+Micronesia, Fed. Sts.
+Gabon
+United Kingdom
+Georgia
+Ghana
+Gibraltar
+Guinea
+Gambia
+Guinea-Bissau
+Equatorial Guinea
+Greece
+Grenada
+Greenland
+Guatemala
+Guam
+Guyana
+High income
+Hong Kong, China
+Honduras
+Heavily indebted poor countries (HIPC)
+Croatia
+Haiti
+Hungary
+IBRD only
+IDA & IBRD total
+IDA total
+IDA blend
+Indonesia
+IDA only
+Isle of Man
+India
+Not classified
+Ireland
+Iran
+Iraq
+Iceland
+Israel
+Italy
+Jamaica
+Jordan
+Japan
+Kazakhstan
+Kenya
+Kyrgyz Republic
+Cambodia
+Kiribati
+St. Kitts and Nevis
+South Korea
+Kuwait
+Latin America & Caribbean (excluding high income)
+Lao
+Lebanon
+Liberia
+Libya
+St. Lucia
+Latin America & Caribbean
+Least developed countries: UN classification
+Low income
+Liechtenstein
+Sri Lanka
+Lower middle income
+Low & middle income
+Lesotho
+Late-demographic dividend
+Lithuania
+Luxembourg
+Latvia
+Macao SAR, China
+St. Martin (French part)
+Morocco
+Monaco
+Moldova
+Madagascar
+Maldives
+Middle East & North Africa
+Mexico
+Marshall Islands
+Middle income
+North Macedonia
+Mali
+Malta
+Myanmar
+Middle East & North Africa (excluding high income)
+Montenegro
+Mongolia
+Northern Mariana Islands
+Mozambique
+Mauritania
+Mauritius
+Malawi
+Malaysia
+North America
+Namibia
+New Caledonia
+Niger
+Nigeria
+Nicaragua
+Netherlands
+Norway
+Nepal
+Nauru
+New Zealand
+OECD members
+Oman
+Other small states
+Pakistan
+Panama
+Peru
+Philippines
+Palau
+Papua New Guinea
+Poland
+Pre-demographic dividend
+Puerto Rico
+North Korea
+Portugal
+Paraguay
+West Bank and Gaza
+Pacific island small states
+Post-demographic dividend
+French Polynesia
+Qatar
+Romania
+Russia
+Rwanda
+South Asia
+Saudi Arabia
+Sudan
+Senegal
+Singapore
+Solomon Islands
+Sierra Leone
+El Salvador
+San Marino
+Somalia
+Serbia
+Sub-Saharan Africa (excluding high income)
+South Sudan
+Sub-Saharan Africa
+Small states
+Sao Tome and Principe
+Suriname
+Slovak Republic
+Slovenia
+Sweden
+Eswatini
+Sint Maarten (Dutch part)
+Seychelles
+Syria
+Turks and Caicos Islands
+Chad
+East Asia & Pacific (IDA & IBRD countries)
+Europe & Central Asia (IDA & IBRD countries)
+Togo
+Thailand
+Tajikistan
+Turkmenistan
+Latin America & the Caribbean (IDA & IBRD countries)
+Timor-Leste
+Middle East & North Africa (IDA & IBRD countries)
+Tonga
+South Asia (IDA & IBRD)
+Sub-Saharan Africa (IDA & IBRD countries)
+Trinidad and Tobago
+Tunisia
+Turkey
+Tuvalu
+Tanzania
+Uganda
+Ukraine
+Upper middle income
+Uruguay
+United States
+Uzbekistan
+St. Vincent and the Grenadines
+Venezuela
+British Virgin Islands
+Virgin Islands (U.S.)
+Vietnam
+Vanuatu
+World
+Samoa
+Kosovo
+Yemen
+South Africa
+Zambia
+Zimbabwe
+"""
+gdp_per_capita = """
+1955,006208
+6452,355165
+13364,1554
+
+17570,1376
+75075,25741
+20610,56855
+10343,17559
+
+26868,13352
+51663,36509
+55454,68929
+18044,13678
+744,182072
+51407,99834
+2424,755845
+1984,884306
+4371,754986
+21960,36994
+47303,04851
+32087,72915
+14623,67405
+19994,80408
+8648,0888
+
+7873,166243
+16096,40096
+18554,12928
+80920,04868
+10167,93406
+18615,79334
+859,9356196
+48130,25597
+31214,20902
+68060,94105
+
+25222,52778
+18236,61298
+4207,086934
+3785,079978
+932,1720068
+5662,063411
+15012,93027
+2913,387306
+7454,063917
+17671,09535
+16187,11186
+
+27743,07005
+
+38513,28457
+39743,59709
+53074,54012
+
+11129,86119
+55671,16527
+17748,18532
+15481,78762
+16113,5407
+10371,60394
+19329,7759
+21931,72445
+34702,55472
+11734,38739
+12412,3094
+46207,32351
+
+39715,43906
+35973,77646
+2022,137961
+43398,9481
+4665,188533
+48416,93603
+10879,47846
+45342,39574
+
+3553,061455
+17875,76115
+45973,5735
+12004,57383
+4746,684995
+
+2504,869745
+2612,000995
+1799,067891
+22743,82443
+29592,15242
+15557,54504
+
+8462,374008
+
+8640,73737
+50630,96212
+64596,56385
+5138,752457
+2530,644477
+27579,78083
+1866,617589
+31102,50275
+14749,82774
+12003,29627
+4059,557395
+5614,24668
+13079,6193
+3249,854407
+
+7762,88177
+
+83203,39468
+
+17435,85393
+57303,06078
+39919,16965
+41830,42633
+9326,664059
+9478,904588
+42797,45852
+27879,75195
+3467,556478
+3884,708809
+4360,854404
+2293,920333
+30207,79021
+40111,77576
+72897,56235
+16017,99609
+7439,59936
+13081,10746
+1308,629536
+20764,16414
+13881,37213
+16590,22225
+2995,447758
+2287,806888
+
+13473,66309
+7655,137875
+11793,53738
+3219,457157
+19073,35995
+35461,3591
+113337,4205
+30304,8525
+123892,1664
+
+8611,709833
+
+7271,641919
+1891,320668
+15307,72194
+20712,43251
+19844,64567
+3989,037658
+12983,19953
+16358,66205
+2316,6552
+42581,09786
+6674,028647
+
+20689,98675
+13799,90485
+
+1459,698413
+4150,965246
+23750,98804
+1310,995555
+31782,15336
+61303,19065
+11101,82628
+
+1063,421841
+5990,850432
+5533,551064
+56328,94114
+65510,58527
+3089,556352
+16504,25793
+41005,42087
+45623,90308
+41859,93448
+27067,52837
+5567,055608
+25553,69671
+14418,07067
+8951,085654
+18510,21566
+4336,244657
+31336,6035
+3985,944915
+39540,58693
+
+33415,4379
+13599,92656
+5157,568578
+6330,82474
+48705,20745
+
+126898,4259
+28206,35705
+27588,12544
+2251,556388
+7084,490975
+55335,67959
+4767,689827
+3782,538613
+101531,6302
+2422,819533
+1601,974029
+8331,804142
+
+
+17434,91613
+3985,118728
+
+3987,503987
+23836,72369
+3418,58693
+15510,46464
+33736,40094
+38048,78466
+53208,88436
+10637,84173
+
+30557,07572
+
+25370,73354
+1968,258262
+16282,93743
+22758,66419
+1773,896632
+19051,33338
+3449,779316
+19304,10131
+16510,95875
+7658,434996
+
+6419,561778
+7084,490975
+3987,503987
+32014,78313
+12502,82019
+28068,85941
+4049,56438
+3240,262937
+2038,073892
+9249,462135
+19028,86276
+23572,17746
+62794,58565
+8556,051512
+12287,9755
+
+
+
+7447,814334
+3221,149823
+17948,41704
+6483,534228
+11348,36345
+2575,126385
+13686,88236
+4223,906936
+3029,793005
+"""
+
+countries = countries.split("\n")
+gdp_per_capita = gdp_per_capita.split("\n")
+none_data_id = []
+for i, gdp in enumerate(gdp_per_capita):
+    if gdp is "":
+        none_data_id.append(i)
+
+for i in none_data_id[::-1]:
+    gdp_per_capita.pop(i)
+    countries.pop(i)
+data_gdp = dict()
+for i in range(len(countries)):
+    data_gdp.update({countries[i]: float(gdp_per_capita[i].replace(",", "."))})
+
+democracy_index = """Afghanistan	29,7
+Albania	59,8
+Algeria	35
+Angola	36,2
+Argentina	70,2
+Armenia	47,9
+Australia	90,9
+Austria	82,9
+Azerbaijan	26,5
+Bahrain	27,1
+Bangladesh	55,7
+Belarus	31,3
+Belgium	77,8
+Benin	57,4
+Bhutan	53
+Bolivia	57
+Bosnia and Herzegovina	49,8
+Botswana	78,1
+Brazil	69,7
+Bulgaria	70,3
+Burkina Faso	47,5
+Burundi	23,3
+Cambodia	35,9
+Cameroon	32,8
+Canada	91,5
+Cape Verde	78,8
+Central African Republic	15,2
+Chad	16,1
+Chile	79,7
+China	33,2
+Colombia	69,6
+Comoros	37,1
+Congo, Dem. Rep.	14,9
+Congo, Rep.	33,1
+Costa Rica	80,7
+Cote d'Ivoire	41,5
+Croatia	65,7
+Cuba	30
+Cyprus	75,9
+Czech Republic	76,9
+Denmark	92,2
+Djibouti	28,7
+Dominican Republic	65,4
+Ecuador	62,7
+Egypt	33,6
+El Salvador	59,6
+Equatorial Guinea	19,2
+Eritrea	23,7
+Estonia	79,7
+Ethiopia	33,5
+Fiji	58,5
+Finland	91,4
+France	78
+Gabon	36,1
+Gambia	43,1
+Georgia	55
+Germany	86,8
+Ghana	66,3
+Greece	72,9
+Guatemala	56
+Guinea	31,4
+Guinea-Bissau	19,8
+Guyana	66,7
+Haiti	49,1
+Honduras	56,3
+Hong Kong, China	61,5
+Hungary	66,3
+Iceland	95,8
+India	72,3
+Indonesia	63,9
+Iran	24,5
+Iraq	40,6
+Ireland	91,5
+Israel	77,9
+Italy	77,1
+Jamaica	70,2
+Japan	79,9
+Jordan	39,3
+Kazakhstan	29,4
+Kenya	51,1
+Kuwait	38,5
+Kyrgyz Republic	51,1
+Lao	23,7
+Latvia	73,8
+Lebanon	46,3
+Lesotho	66,4
+Liberia	53,5
+Libya	21,9
+Lithuania	75
+Luxembourg	88,1
+North Macedonia	58,7
+Madagascar	52,2
+Malawi	54,9
+Malaysia	68,8
+Mali	54,1
+Malta	82,1
+Mauritania	38,2
+Mauritius	82,2
+Mexico	61,9
+Moldova	58,5
+Mongolia	65
+Montenegro	57,4
+Morocco	49,9
+Mozambique	38,5
+Myanmar	38,3
+Namibia	62,5
+Nepal	51,8
+Netherlands	88,9
+New Zealand	92,6
+Nicaragua	36,3
+Niger	37,6
+Nigeria	44,4
+North Korea	10,8
+Norway	98,7
+Oman	30,4
+Pakistan	41,7
+Palestine	43,9
+Panama	70,5
+Papua New Guinea	60,3
+Paraguay	62,4
+Peru	66
+Philippines	67,1
+Poland	66,7
+Portugal	78,4
+Qatar	31,9
+Romania	63,8
+Russia	29,4
+Rwanda	33,5
+Saudi Arabia	19,3
+Senegal	61,5
+Serbia	64,1
+Sierra Leone	46,6
+Singapore	63,8
+Slovak Republic	71
+Slovenia	75
+South Africa	72,4
+South Korea	80
+Spain	80,8
+Sri Lanka	61,9
+Sudan	21,5
+Suriname	69,8
+Swaziland	30,3
+Sweden	93,9
+Switzerland	90,3
+Syria	14,3
+Taiwan	77,3
+Tajikistan	19,3
+Tanzania	54,1
+Thailand	46,3
+Timor-Leste	71,9
+Togo	31
+Trinidad and Tobago	71,6
+Tunisia	64,1
+Turkey	43,7
+Turkmenistan	17,2
+Uganda	52
+Ukraine	56,9
+United Arab Emirates	27,6
+United Kingdom	85,3
+United States	79,6
+Uruguay	83,8
+Uzbekistan	20,1
+Venezuela	31,6
+Vietnam	30,8
+Yemen	19,5
+Zambia	56,1
+Zimbabwe	31,6""".split("\n")
+democracy_data = dict()
+for elem in democracy_index:
+    elems = elem.split("\t")
+    democracy_data.update({elems[0]: float(elems[1].replace(",", "."))})
+    
+#print(democracy_data)
+#print(data_gdp)
+
+full_democracies = dict() # d > 80.0
+flawed_democracies = dict() # d > 60.0
+hybrid_regimes = dict() # d > 40.0
+authoritarian = dict()
+
+
+for k, v in democracy_data.items():
+    try:
+        #print(k, data_gdp[k])
+        if v > 80.0:
+            full_democracies.update({k: data_gdp[k]})
+        elif v > 60.0:
+            flawed_democracies.update({k: data_gdp[k]})
+        elif v > 40.0:
+            hybrid_regimes.update({k: data_gdp[k]})
+        else:
+            authoritarian.update({k: data_gdp[k]})
+    except KeyError: print("No GDP data:", k)
+
+full_avg_gdp = 0.0
+flawed_avg_gdp = 0.0
+hybrid_avg_gdp = 0.0
+authoritarian_avg_gdp = 0.0
+
+minimal_gdp = [["", max(data_gdp.values())] for x in range(4)]
+
+for i, k in enumerate(full_democracies.keys()):
+    v = data_gdp[k]
+    full_avg_gdp += v
+    if minimal_gdp[0][1] > v:
+               minimal_gdp[0] = (k, v)
+i += 1
+full_avg_gdp /= i
+
+for i, k in enumerate(flawed_democracies.keys()):
+    v = data_gdp[k]
+    flawed_avg_gdp += data_gdp[k]
+    if minimal_gdp[1][1] > v:
+               minimal_gdp[1] = (k, v)
+i += 1
+flawed_avg_gdp /= i
+
+for i, k in enumerate(hybrid_regimes.keys()):
+    v = data_gdp[k]
+    hybrid_avg_gdp += v
+    if minimal_gdp[2][1] > v:
+               minimal_gdp[2] = (k, v)
+i += 1
+hybrid_avg_gdp /= i
+
+for i, k in enumerate(authoritarian.keys()):
+    v = data_gdp[k]
+    authoritarian_avg_gdp += v
+    if minimal_gdp[3][1] > v:
+               minimal_gdp[3] = (k, v)
+i += 1
+authoritarian_avg_gdp /= i
+
+print("Average values GDP per capita")
+print("Full democracies:", "{:,.0f}$".format(full_avg_gdp))
+print("Flawed democracies:", "{:,.0f}$".format(flawed_avg_gdp))
+print("Hybrid regimes:", "{:,.0f}$".format(hybrid_avg_gdp))
+print("Authoritarian regimes:", "{:,.0f}$".format(authoritarian_avg_gdp))
+print("Minimal GDP per capita for full, flawed, hybrid and authoritarian:\n", minimal_gdp)
+
